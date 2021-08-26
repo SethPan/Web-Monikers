1b58be4265f8788a1ebb49b5ea94f7eb62f3185b
Seth Panousis <stpanousis@comcast.net>
2021-08-26 16:44:25 -0400

    simple chat socket created

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,30 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
 const port = 3050;
-const server = require("http").createServer(app);
 const cors = require("cors");
-const io = require("socket.io")(server);
-io.on("connection", () => {
-  /* … */
-});
+const { Server } = require("socket.io");
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+const io = new Server(server);
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
 
 server.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
