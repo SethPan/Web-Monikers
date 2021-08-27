88519c3c26c4ff5cd015e8078e4870053ee3bd2c
Seth Panousis <stpanousis@comcast.net>
2021-08-27 17:50:57 -0400

    some basic functionality

--- backend/index.js
+++ backend/index.js
@@ -14,15 +14,53 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+const cards = [
+  { title: 1, description: 1, value: 1 },
+  { title: 2, description: 2, value: 2 },
+  { title: 3, description: 3, value: 3 },
+  { title: 4, description: 4, value: 4 },
+  { title: 5, description: 5, value: 5 },
+  { title: 6, description: 6, value: 6 },
+  { title: 7, description: 7, value: 7 },
+  { title: 8, description: 8, value: 8 },
+];
+let cardIndex = 0;
+
 const io = new Server(server);
 io.on("connection", (socket) => {
+  let gameActive = false;
+
   console.log("a user connected");
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
 
   socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+    if (gameActive) {
+      io.emit("chat message", msg);
+    } else {
+      io.emit("chat message", "The game has not started yet.");
+    }
+  });
+
+  socket.on("start game", () => {
+    io.emit("start game");
+    gameActive = true;
+    io.emit("new card", cards[cardIndex]);
+    cardIndex++;
+    if (cardIndex > cards.length - 1) {
+      cardIndex = 0;
+    }
+  });
+
+  socket.on("new card", () => {
+    if (gameActive) {
+      io.emit("new card", cards[cardIndex]);
+      cardIndex++;
+      if (cardIndex > cards.length - 1) {
+        cardIndex = 0;
+      }
+    }
   });
 });
 
