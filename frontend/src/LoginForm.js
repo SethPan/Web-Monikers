bbe2a6e4ecdab81e83f760b1755761f1a5d31824
Seth Panousis <stpanousis@comcast.net>
2021-09-07 16:15:00 -0400

    style implemented

--- frontend/src/LoginForm.js
+++ frontend/src/LoginForm.js
@@ -8,7 +8,7 @@ function LoginForm() {
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
+          <Form.Text className="text-muted" id="italic">
             We'll never share your email with anyone else.
           </Form.Text>
         </Form.Group>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
+import "bootswatch/dist/darkly/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import LoginForm from "./LoginForm";
-
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
