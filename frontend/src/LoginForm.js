f45e1b0a10f5f637831bee22bfadc87c3f9202e2
Seth Panousis <stpanousis@gmail.com>
2021-10-27 14:25:31 -0400

    react conditional rendering beginnig

--- frontend/src/LoginForm.js
+++ frontend/src/LoginForm.js
@@ -6,19 +6,19 @@ import axios from "axios";
 import NewUser from "./NewUser.js";
 
 function LoginForm(props) {
-  // console.log(props);
+  //state
   const [emailText, setEmailText] = useState("");
   const [passwordText, setPasswordText] = useState("");
   const [newUser, setNewUser] = useState(false);
 
   //to replace login jsx with NewUser component
-  if (newUser) {
+  if (newUser) { currentComponent = NewUser
   }
 
+  //handle text state
   function handleEmailTyping(event) {
     setEmailText(event.target.value);
   }
-
   function handlePasswordTyping(event) {
     setPasswordText(event.target.value);
   }
@@ -43,20 +43,18 @@ function LoginForm(props) {
       handleSubmit();
     }
   }
-
   function submitButton(event) {
     event.preventDefault();
     handleSubmit();
   }
-
   function handleNewAccount(event) {
     event.preventDefault();
     setNewUser(true);
   }
 
+  //to possibly impliment later
   function handleGoogleOAuth(event) {
     event.preventDefault();
-
     // //temporarily disabled
     // axios
     //   .get("http://localhost:3050/auth/google")
@@ -69,48 +67,53 @@ function LoginForm(props) {
     //   .then(function () {});
   }
 
+  // default component for login screen
+  const defaultLogin = <div>
+  <Form>
+    <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Label>Email address</Form.Label>
+      <Form.Control
+        type="email"
+        placeholder="Enter email"
+        value={emailText}
+        onChange={handleEmailTyping}
+        onKeyPress={handleKeyPress}
+      />
+      <Form.Text className="text-muted" id="italic"></Form.Text>
+    </Form.Group>
+
+    <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Label>Password</Form.Label>
+      <Form.Control
+        type="password"
+        placeholder="Password"
+        value={passwordText}
+        onChange={handlePasswordTyping}
+        onKeyPress={handleKeyPress}
+      />
+    </Form.Group>
+    {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
+      <Form.Check type="checkbox" label="Check me out" />
+    </Form.Group> */}
+    <Button variant="primary" type="submit" onClick={submitButton}>
+      Submit
+    </Button>
+    <Button variant="secondary" type="submit" onClick={handleNewAccount}>
+      New Account
+    </Button>
+  </Form>
+
+  <div className="mb-2">
+    <Button variant="primary" size="lg" onClick={handleGoogleOAuth}>
+      Log in with Google
+    </Button>{" "}
+  </div>
+</div>;
+
+let currentComponent = defaultLogin
+
   return (
-    <div>
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            value={emailText}
-            onChange={handleEmailTyping}
-            onKeyPress={handleKeyPress}
-          />
-          <Form.Text className="text-muted" id="italic"></Form.Text>
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            value={passwordText}
-            onChange={handlePasswordTyping}
-            onKeyPress={handleKeyPress}
-          />
-        </Form.Group>
-        {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group> */}
-        <Button variant="primary" type="submit" onClick={submitButton}>
-          Submit
-        </Button>
-        <Button variant="secondary" type="submit" onClick={handleNewAccount}>
-          New Account
-        </Button>
-      </Form>
-
-      <div className="mb-2">
-        <Button variant="primary" size="lg" onClick={handleGoogleOAuth}>
-          Log in with Google
-        </Button>{" "}
-      </div>
-    </div>
+    defaultLogin
   );
 }
 
