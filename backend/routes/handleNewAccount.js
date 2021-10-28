73871d5ea755e60667faa395f0fb9e3d2320b2b7
Seth Panousis <panousisseth@gmail.com>
2021-10-28 16:07:09 -0400

    building, tweaking, checking for email before registering new account

--- backend/routes/handleNewAccount.js
+++ backend/routes/handleNewAccount.js
@@ -1,14 +1,35 @@
 const { Pool, Client } = require("pg");
-
 const connectionString =
-  "postgressql://postgres:password@localhost:5432/WebMonikers";
+  "postgressql://postgres:password@localhost:5432/webmonikers";
 
-function handleNewAccount() {
+function handleNewAccount(req, res) {
   const client = new Client({
     connectionString: connectionString,
   });
   console.log("\nhandleNewAccount running \n");
   client.connect();
+
+  //input fields sent from client
+  const email = req.body.email;
+  const password = req.body.password;
+
+  client.query(
+    `SELECT email FROM users
+  WHERE email = lower('${email}')`,
+    (err, resp) => {
+      if (err) {
+        console.log(
+          "\nhandle login error\n",
+          err
+        );
+      } else {
+        if (resp.rows[0].email === email) {
+          console.log("dupe")
+          res.send("duplicate");
+        }
+      }
+    }
+  );
 }
 
 module.exports = handleNewAccount;
